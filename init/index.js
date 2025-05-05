14b94382f985aa65e4b84518b094e9ee0f792cad
chaudharyrizwan <rizwanmd293888@gmail.com>
2025-05-06 01:35:39 +0530

    Add project files and fix line endings

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose= require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const Mongo_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(()=>{
+    console.log("connected to db");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(Mongo_URL);
+}
+
+const initDB = async ()=>{
+   await Listing.deleteMany({});
+   initData.data = initData.data.map((obj)=>({
+    ...obj,
+    owner:"66e0b56a62eab14089233829"
+   }));
+   await Listing.insertMany(initData.data);
+   console.log("data was initialized");
+};
+
+initDB();
---
+++ routes/bookings.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const Booking = require('../models/booking');
+const Listing = require('../models/listing');
+const { isLoggedIn } = require('../middleware');
+
+router.post('/:id/book', isLoggedIn, async (req, res) => {
+  const listing = await Listing.findById(req.params.id);
+  const { checkIn, checkOut, guests } = req.body;
+
+  const checkInDate = new Date(checkIn);
+  const checkOutDate = new Date(checkOut);
+  const nights = Math.ceil((checkOutDate - checkInDate) / (1000 * 60 * 60 * 24));
+  const basePrice = listing.price * nights;
+  const gst = Math.round(basePrice * 0.18);
+  const totalPrice = basePrice + gst;
+
+  const booking = new Booking({
+    listing,
+    user: req.user._id,
+    checkIn: checkInDate,
+    checkOut: checkOutDate,
+    guests,
+    totalPrice,
+    gst
+  });
+
+  await booking.save();
+  req.flash("success", "Your booking was successful!");
+  res.redirect(`/listings/${listing._id}`);
+});
+
+module.exports = router;
